51d03ee332ff522cf807d20c7b6a43686ec5b08d
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-01-27 21:10:00 -0800

    stats updated not finished

---
+++ tools/overlay/RadomizeStats.gd
@@ -0,0 +1,11 @@
+extends Button
+
+@onready var PLAYER_STATS : PlayerStats = preload("res://resources/stats/player_stats.tres")
+@onready var stats_list : Array = [PLAYER_STATS.wisdom, PLAYER_STATS.intelligence, PLAYER_STATS.charisma, PLAYER_STATS.wellness]
+
+signal stats_changed
+
+func _on_pressed():
+	for stats_index in stats_list.size():
+		stats_list[stats_index] = randi_range(0, 100)
+	emit_signal("stats_changed")
--- tools/overlay/overlay.gd
+++ tools/overlay/overlay.gd
@@ -1,20 +1,33 @@
 extends Control
 
 @onready var PLAYER_STATS : PlayerStats = preload("res://resources/stats/player_stats.tres")
+@onready var STATS_LIST : Array = [PLAYER_STATS.wisdom, PLAYER_STATS.intelligence, PLAYER_STATS.charisma, PLAYER_STATS.wellness]
 
 @onready var stats_container = $StatsContainer
-@onready var wisdom_bar = $StatsContainer/Wisdom
-@onready var wisdom_label = $StatsContainer/Wisdom/Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	wisdom_bar.value = 0
-	var a = create_tween()
-	a.tween_property(wisdom_bar, "value", PLAYER_STATS.wisdom, 0.2)
-	await a.finished
-	wisdom_label.text = str(PLAYER_STATS.wisdom)
+	stats_bar_update()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func stats_bar_update():
+	fetch_stats()
+	for bar_index in stats_container.get_child_count():
+		var current_bar : ProgressBar = stats_container.get_child(bar_index)
+		var a = create_tween()
+		a.tween_property(current_bar, "value", STATS_LIST[bar_index], 0.2)
+		current_bar.get_child(0).text = str(STATS_LIST[bar_index])
+
+
+func _on_radomize_stats_stats_changed():
+	stats_bar_update()
+
+func fetch_stats():
+	PLAYER_STATS = load("res://resources/stats/player_stats.tres")
+	STATS_LIST = [PLAYER_STATS.wisdom, PLAYER_STATS.intelligence, PLAYER_STATS.charisma, PLAYER_STATS.wellness]
