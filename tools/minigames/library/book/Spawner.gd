04c69eaf26c806d6820e1173b96b362ec99d64a2
branden <brandenbohrnsen@gmail.com>
2024-01-30 14:46:00 -0800

    tomfoolery

--- tools/minigames/library/book/Spawner.gd
+++ tools/minigames/library/book/Spawner.gd
@@ -11,5 +11,4 @@ func _process(delta: float) -> void:
 		var book : Book = BOOK.instantiate()
 		book.global_position = Vector3(randf_range(-5, 5), global_position.y, randf_range(-5, 5))
 		add_child(book)
-	
 	time += delta
