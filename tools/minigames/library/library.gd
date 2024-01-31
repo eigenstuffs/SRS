31534f2dcecbbea2dc40aefc56c5b5a93f710113
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-01-31 00:34:05 -0800

    Library game score UI finished

--- tools/minigames/library/library.gd
+++ tools/minigames/library/library.gd
@@ -6,8 +6,35 @@ signal minigame_finished(num_points : int)
 
 @export var points : int = 0
 
+@onready var score_label : Label = $CanvasLayer/Overlay/ScoreLabel
+
 func _ready():
-	pass
+	points = 0
+	update_score()
+
+func update_score():
+	score_label.text = str(points)
+
+#by default we gain and lose by 1 point. If we want to make some
+#extra valuable books or damaging bombs, we can adjust that
+#when calling the gain and lose points function
+func gain_points(increment : int = 1):
+	points += increment
+	
+func lose_points(decrease : int = 1):
+	points -= decrease
 
 func end():
 	emit_signal("minigame_finished", points)
+	print(points)
+
+func _on_increase_point_pressed():
+	gain_points()
+	update_score()
+
+func _on_decrease_point_pressed():
+	lose_points()
+	update_score()
+
+func _on_timers_game_over():
+	end()
