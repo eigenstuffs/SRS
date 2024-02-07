bb75b40f9da4cd62669c6fc786151d0d62795492
branden <88228182+eigenstuffs@users.noreply.github.com>
2024-02-06 22:01:49 -0800

    fish updates

--- tools/minigames/fishing/fish/Fish.gd
+++ tools/minigames/fishing/fish/Fish.gd
@@ -9,29 +9,15 @@ enum STATES { WANDER, WAIT }
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 #var direction = Vector3.ZERO
 var state: STATES = STATES.WANDER
-var turning = false
 
 
 func _physics_process(delta):
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-	
 	if state == STATES.WANDER:
 		velocity = Vector3.FORWARD * SPEED
 		velocity = velocity.rotated(Vector3.UP, rotation.y)
 	elif state == STATES.WAIT:
 		velocity.x = 0
 		velocity.z = 0
-	
-	if not turning:
-		var a = create_tween()
-		turning = true
-		a.tween_property($Sprite3D, "position", position + Vector3(0,.1,0), 0.1)
-		await a.finished
-		a = create_tween()
-		a.tween_property($Sprite3D, "position", position - Vector3(0,.1,0), 0.1)
-		await a.finished
-		turning = false
 		
 	move_and_slide()
 
@@ -45,6 +31,7 @@ func _on_timer_timeout():
 		#is this line needed??
 		rotation = rotation.normalized()
 		rotate_y(randf_range(0, 2*PI))
+	$Timer.start(3)
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
 	pass
--- tools/minigames/fishing/fish/fish_instancer.gd
+++ tools/minigames/fishing/fish/fish_instancer.gd
@@ -2,10 +2,6 @@ extends Node3D
 
 const FISH = preload('res://tools/minigames/fishing/fish/fish.tscn')
 
-@export var spawn_interval_seconds : float = 1.0
-
-var time : float = 0
-var spawn_time_next : float = 0
 var active = true
 
 @onready var area_left : SpawnArea = $SpawnAreaLeft
@@ -15,24 +11,20 @@ var active = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #h00 4 functions, override return
+
 func _process(delta: float) -> void:
-	if active and get_children().size()<3:
-		if time >= spawn_time_next:
-			spawn_time_next += spawn_interval_seconds
-			
-			var spawn_pos
-			var side = randi_range(0,4)
-			match side:
-				0:
-					spawn_pos = area_left.get_random_point()
-				1:
-					spawn_pos = area_right.get_random_point()
-				2:
-					spawn_pos = area_bottom.get_random_point()
-				3:
-					spawn_pos = area_top.get_random_point()
-			var fishie: Fish = FISH.instantiate()
-			add_child(fishie)
-			fishie.global_position = spawn_pos
-			
-		time += delta
+	if active and $fish.get_child_count() <= 3:
+		var spawn_pos
+		var side = randi_range(0,3)
+		match side:
+			0:
+				spawn_pos = area_left.get_random_point()
+			1:
+				spawn_pos = area_right.get_random_point()
+			2:
+				spawn_pos = area_bottom.get_random_point()
+			3:
+				spawn_pos = area_top.get_random_point()
+		var fishie: Fish = FISH.instantiate()
+		$fish.add_child(fishie)
+		fishie.global_position = spawn_pos
