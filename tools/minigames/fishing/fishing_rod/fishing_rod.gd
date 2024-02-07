638a7666572124d29e9c75231ff1a95a6bbdfb9a
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-02-07 01:17:56 -0800

    working but not complete rod

--- tools/minigames/fishing/fishing_rod/fishing_rod.gd
+++ tools/minigames/fishing/fishing_rod/fishing_rod.gd
@@ -3,13 +3,21 @@ extends MeshInstance3D
 @onready var bobber : RigidBody3D = $Bobber
 
 var bobber_v : Vector3 = Vector3.ZERO
-var gravity : Vector3 = Vector3.DOWN * 2
+var gravity : Vector3 = Vector3.DOWN * 9.8
 var activated : int = 0
+var bobber_detached : bool = false
+var bobber_pos
 
+func _ready():
+	bobber_pos = bobber.transform
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_accept"):
-		bobber_v = Vector3(1, 2, 0)
-		activated = 1
-	bobber_v += activated * gravity * delta
-	bobber.transform.origin += bobber_v * delta
+	bobber_v += gravity * delta
+	if Input.is_action_just_pressed("ui_accept"):
+		if bobber_detached == false:
+			bobber_v = Vector3(0, 3, 3.5)
+			bobber_detached = true
+		else:
+			bobber.transform = bobber_pos
+			bobber_detached = false
+	bobber.linear_velocity = bobber_v
