7ce9d62baeaaa2d9c1d7f1f0128ec05d29f0c717
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-02-11 01:49:18 -0800

    better bobbers
    Former-commit-id: 2470bd4a1afb12ab77a64a59e78a4ae82bf35efd

--- tools/minigames/fishing/fishing_rod/fishing_rod.gd
+++ tools/minigames/fishing/fishing_rod/fishing_rod.gd
@@ -3,26 +3,24 @@ extends MeshInstance3D
 signal fishing_starts
 signal fishing_ends
 
-@onready var bobber : RigidBody3D = $Bobber
+@onready var moving_bobber : RigidBody3D = $MovingBobber
+@onready var floating_bobber : RigidBody3D = $FloatingBobber
 
 var bobber_initial_v : Vector3 = Vector3(0, 5, 3)
 var bobber_detached : bool = false
-var bobber_pos
-
-func _ready():
-	bobber_pos = bobber.transform
-	bobber.visible = false
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		if !bobber_detached:
 			bobber_detached = true
-			bobber.transform = bobber_pos
-			bobber.visible = true
-			bobber.launch(bobber_initial_v)
+			moving_bobber.launch(bobber_initial_v)
 			emit_signal("fishing_starts")
-		elif bobber.is_in_water:
+		elif moving_bobber.is_in_water:
 			bobber_detached = false
-			bobber.transform = bobber_pos
-			bobber.visible = false
+			floating_bobber.visible = false
+			floating_bobber.transform = self.transform
 			emit_signal("fishing_ends")
+
+func _on_moving_bobber_water_entered():
+	floating_bobber.transform = moving_bobber.transform
+	floating_bobber.visible = true
