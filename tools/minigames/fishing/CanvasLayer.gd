f3618c00be43fea1720028d982bdd8fb7f20f89a
branden <brandenbohrnsen@gmail.com>
2024-02-13 22:22:52 -0800

    loop fix
    Former-commit-id: 8e6ff9ef60569d65f5cc67d1dcd5add65209fe10

--- tools/minigames/fishing/CanvasLayer.gd
+++ tools/minigames/fishing/CanvasLayer.gd
@@ -6,6 +6,8 @@ var min_force_val = 30
 var max_force_val = 150
 var bar_active : bool = false
 
+signal stop_loop
+
 func _ready():
 	force_bar.value = min_force_val
 	force_bar.visible = false
@@ -14,14 +16,18 @@ func _process(delta):
 	pass
 
 func _on_fishing_player_casting_time():
+	force_bar.value = min_force_val
 	force_bar.visible = true
 	bar_animation()
 
 func bar_animation():
-	force_bar.value = min_force_val
 	var a = create_tween().set_loops()
 	a.tween_property(force_bar, "value", max_force_val, 1)
 	a.tween_property(force_bar, "value", min_force_val, 1)
+	await stop_loop
+	a.kill()
 
 func _on_fishing_player_fishing_time():
+	emit_signal("stop_loop")
 	force_bar.visible = false
+	force_bar.value = min_force_val
