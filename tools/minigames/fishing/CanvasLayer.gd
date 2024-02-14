03fa7fe16a50489697fd57256e7735f59b93d6d5
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-02-13 21:57:07 -0800

    more failed attempt
    Former-commit-id: 73d670e592ef4dd32b5a77ef61821ff96c0e3e1a

--- tools/minigames/fishing/CanvasLayer.gd
+++ tools/minigames/fishing/CanvasLayer.gd
@@ -4,15 +4,24 @@ extends CanvasLayer
 
 var min_force_val = 30
 var max_force_val = 150
-var is_running : bool = false
+var bar_active : bool = false
 
 func _ready():
 	force_bar.value = min_force_val
+	force_bar.visible = false
 
 func _process(delta):
 	pass
 
 func _on_fishing_player_casting_time():
-	var a = create_tween()
+	force_bar.visible = true
+	bar_animation()
+
+func bar_animation():
+	force_bar.value = min_force_val
+	var a = create_tween().set_loops()
 	a.tween_property(force_bar, "value", max_force_val, 1)
 	a.tween_property(force_bar, "value", min_force_val, 1)
+
+func _on_fishing_player_fishing_time():
+	force_bar.visible = false
