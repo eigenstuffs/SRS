b737c11e8f0188f4c568180bc3696ed5e7847d62
Ethan Truong <18603664+2Retr0@users.noreply.github.com>
2024-02-17 19:38:30 -0800

    Add score and hit feedback for rhythm minigame
    Former-commit-id: 43e57a0cac8e55d9de2f1e7e77947c48f58a52c8

---
+++ tools/minigames/rhythm/assets/fumo/fumofumo.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+var pulse_time_start : float = -1
+var time : float = 0
+var scale_delta : float = 0.6
+
+@onready var start_scale : float = scale.z
+
+func _process(delta: float) -> void:
+	time += delta
+	
+	rotation.z += deg_to_rad(180 * delta)
+	if pulse_time_start != -1:
+		var pulse_time_elapsed = time - pulse_time_start
+		scale.z = start_scale * (1.0 - smoothstep(1, 0, abs(pulse_time_elapsed - 0.1))) * scale_delta + (1.0 - scale_delta)
+
+func _on_audio_synchronizer_on_beat() -> void:
+	pulse_time_start = time
--- tools/minigames/rhythm/play/playfield.gd
+++ tools/minigames/rhythm/play/playfield.gd
@@ -5,8 +5,12 @@ class_name RhythmPlayfield extends Node3D
 ## Will dynamically initialize an appropriate amount of [class RhythmKey [class AudioSynchronizer] members based on its own
 ## members.
 
+const PHYSICS_TEXT = preload('res://tools/minigames/rhythm/util/physics_text.tscn')
 const KEY = preload('res://tools/minigames/rhythm/play/key.tscn')
-const PLAYFIELD_WIDTH = 8.0 # FIXME: No more fixed width please!
+const PLAYFIELD_WIDTH : float = 8.0 # FIXME: No more fixed width please!
+const RED : Color = Color(0.851, 0.184, 0.251)
+const GREEN : Color = Color(0.424, 0.851, 0.475)
+const BLUE : Color = Color(0.306, 0.796, 0.851)
 
 @export var stream : AudioStream
 @export var note_speed : float = 2
@@ -28,10 +32,14 @@ func _ready() -> void:
 	for key_index in range(audio_synchronizer.beatmap.num_keys):
 		var key : RhythmKey = KEY.instantiate()
 		key.position.x = (-0.5 + key_index / (audio_synchronizer.beatmap.num_keys - 1.0))*PLAYFIELD_WIDTH
-		# FIXME : Need more dynamic keybind system for more than 4 keys!
+		# FIXME: I thought we were supposed to stick to the built-in input mappings!
 		match audio_synchronizer.beatmap.num_keys:
-			4: key.keybind = {0: 'ui_left', 1: 'ui_down', 2: 'ui_up', 3: 'ui_right'}[key_index]
-			7: key.keybind = {0: 'ui_left', 1: 'ui_left', 2: 'ui_down', 3: 'ui_accept', 4: 'ui_up', 5: 'ui_right', 6: 'ui_right'}[key_index]
+			4:
+				key.keybind = 'rhythm_key_%d' % key_index
+				key.note_color_overwrite = [GREEN, BLUE, BLUE, GREEN][key_index]
+			7: 
+				key.keybind = 'rhythm_key_%d' % key_index
+				key.note_color_overwrite = [GREEN, BLUE, GREEN, RED, GREEN, BLUE, GREEN][key_index]
 			_: assert(false, 'Playfield encountered an unsupported key count!')
 		key.audio_synchronizer = audio_synchronizer
 		key.scroll_time = 1.0 / note_speed
@@ -55,21 +63,39 @@ func _on_key_report_hit(timing_offset : Variant, hit_type : Note.HitType):
 			var offset_ms = abs(timing_offset*1e3)
 			if offset_ms < 70:
 				print('(playfield) got score: perfect! (%sms)' % offset_ms)
+				create_score_text('Perfect!', Color(0.231, 0.604, 1))
 			elif offset_ms < 100:
 				print('(playfield) got score: great!   (%sms)' % offset_ms)
+				create_score_text('Great!', Color(0.31, 0.89, 0.369))
 			elif offset_ms < 150:
 				print('(playfield) got score: okay.    (%sms)' % offset_ms)
+				create_score_text('Okay.', Color(0.73, 0.58, 0.84))
 			else:
 				print('(playfield) got score: bad      (%sms)' % offset_ms)
+				create_score_text('Bad', Color(0.71, 0.565, 0.659))
 		Note.HitType.RELEASE:
 			var offset_ms = abs(timing_offset*1e3)
 			if offset_ms < 100:
 				print('(playfield) got score: perfect! (%sms)' % offset_ms)
+				create_score_text('Perfect!', Color(0.231, 0.604, 1))
 			elif offset_ms < 150:
 				print('(playfield) got score: great!   (%sms)' % offset_ms)
+				create_score_text('Great!', Color(0.31, 0.89, 0.369))
 			elif offset_ms < 200:
 				print('(playfield) got score: okay.    (%sms)' % offset_ms)
+				create_score_text('Okay.', Color(0.73, 0.58, 0.84))
 			else:
 				print('(playfield) got score: bad      (%sms)' % offset_ms)
+				create_score_text('Bad', Color(0.71, 0.565, 0.659))
 		Note.HitType.MISS:
 			print('(playfield) got score: missed!')
+			create_score_text('Missed!', Color(0.89, 0.25, 0.27))
+
+func create_score_text(text : String, color : Color):
+	var score : PhysicsText = PHYSICS_TEXT.instantiate()
+	score.force = Vector2(randf_range(-0.75, 0.75), randf_range(5, 6.25))
+	score.fake_torque = randf_range(-0.25, 0.25)
+	score.text = text
+	score.text_color = color
+	score.position.x = randi_range(125, 225)
+	add_child(score)
