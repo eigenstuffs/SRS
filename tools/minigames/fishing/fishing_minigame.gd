28778ead866e718913aad58a4ca2263afbe3b3fc
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-02-20 23:20:43 -0800

    fish rarity texture implemented
    Former-commit-id: 913c36a38de736e62e2acd3df58ba8e865653f00

--- tools/minigames/fishing/fishing_minigame.gd
+++ tools/minigames/fishing/fishing_minigame.gd
@@ -8,12 +8,13 @@ signal reeling_minigame
 signal reeling_minigame_end(is_successful : bool)
 
 var force_multiplier : float = 1
+var fish_caught : int = 0
 
 func _ready():
 	await get_tree().create_timer(3).timeout # wait for countdown timer
 	
 func end() -> void:
-	pass # override the minigame.gd function for game-specific calls
+	emit_signal("minigame_finished", fish_caught)
 	# see library_minigame.gd for example
 	
 func _process(delta):
@@ -23,7 +24,9 @@ func _process(delta):
 
 func _on_canvas_layer_reeling_ended(is_successful):
 	emit_signal("reeling_minigame_end", is_successful)
-	print("reeling success: " + str(is_successful) + "!")
+	if is_successful:
+		fish_caught += 1
+		update_points.emit(fish_caught)
 
 func _on_fish_instancer_fish_hooked():
 	emit_signal("reeling_minigame")
