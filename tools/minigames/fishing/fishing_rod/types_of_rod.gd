f475a1e39c1a0419672bdbe2c4d5fc6d5fbd6a86
lconfusion <lchengalt@gmail.com>
2024-02-26 19:08:07 -0800

    more fishing rods + took out sell/buy price & held values
    haven't added reel speed or reel size yet

--- tools/minigames/fishing/fishing_rod/types_of_rod.gd
+++ tools/minigames/fishing/fishing_rod/types_of_rod.gd
@@ -2,9 +2,13 @@ extends Resource
 
 class_name FishingRodTypes
 
-@export var rod_one : Dictionary = {"name": "Default Fishing Rod", "sell_price": 250, "buy_price": 300, "description": "standard fishing rod. it catches fish.", "held": 1}
-@export var rod_two : Dictionary = {"name": "Cooler Fishing Rod", "sell_price": 300, "buy_price": 350, "description": "a fishing rod, but cooler.", "held": 1}
-@export var rod_list : Array[Dictionary] = [rod_one,rod_two]
+@export var rod_one : Dictionary = {"name": "Default Fishing Rod", "description": "standard fishing rod. it catches fish."}
+#"sell_price": 250, "buy_price": 300, "held": 1
+@export var rod_two : Dictionary = {"name": "Cooler Fishing Rod", "description": "a fishing rod, but cooler."}
+#"sell_price": 300, "buy_price": 350, "held": 1
+@export var rod_three: Dictionary = {"name": "Superior Fishing Rod", "description": "they say that this is the rod of the masters."}
+@export var rod_four: Dictionary = {"name": "BEST Fishing Rod", "description": "a rod with faded lettering on the handle. 'Cult de Poisson'...?"}
+@export var rod_list : Array[Dictionary] = [rod_one,rod_two, rod_three, rod_four]
 
 
 
@@ -13,6 +17,6 @@ class_name FishingRodTypes
 func set_rod_stats(rod_type : int, rod : InventoryItem):
 	rod.get_node("Description").text = rod_list[rod_type].get("description")
 	rod.get_node("Name").text = rod_list[rod_type].get("name")
-	rod.get_node("SellPrice").text = str(rod_list[rod_type].get("sell_price"))
-	rod.get_node("BuyPrice").text = str(rod_list[rod_type].get("buy_price"))
-	rod.get_node("MaxHeld").text = str(rod_list[rod_type].get("held"))
+	#rod.get_node("SellPrice").text = str(rod_list[rod_type].get("sell_price"))
+	#rod.get_node("BuyPrice").text = str(rod_list[rod_type].get("buy_price"))
+	#rod.get_node("MaxHeld").text = str(rod_list[rod_type].get("held"))
