ff93d6120ade89d46ee31c5152d45ae82431616b
DubiousDuck <118256740+DubiousDuck@users.noreply.github.com>
2024-03-10 01:08:28 -0800

    minor improvements of fishing game

--- tools/minigames/fishing/Inventory/inventory_item.gd
+++ tools/minigames/fishing/Inventory/inventory_item.gd
@@ -12,5 +12,5 @@ class_name InventoryItem
 func show_description(rod_type : int):
 	rod_name.text = ROD_TYPE.rod_list[rod_type].get("name")
 	description.text = ROD_TYPE.rod_list[rod_type].get("description")
-	reel_strength.text = str(ROD_TYPE.rod_list[rod_type].get("reel_strength"))
-	reel_size.text = str(ROD_TYPE.rod_list[rod_type].get("reel_size"))
+	reel_strength.text = "reel strength: " + str(ROD_TYPE.rod_list[rod_type].get("reel_strength"))
+	reel_size.text = "reel size: " + str(ROD_TYPE.rod_list[rod_type].get("reel_size"))
