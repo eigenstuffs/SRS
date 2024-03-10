3695ee2730f4c0d675ebef03b6a2686eccc66964
branden <brandenbohrnsen@gmail.com>
2024-03-10 00:24:12 -0800

    aaaa

--- tools/dialogue/Effects.gd
+++ tools/dialogue/Effects.gd
@@ -1,11 +1,24 @@
 extends Node
 
+@onready var audio = $Music
+@onready var sfx = $SFX
+
+var song
+
 func _ready():
 	var vn : VisualNovelDialogue = get_parent()
 	
 	vn.connect("fade_black", fade_black)
+	vn.connect("fade_red", fade_red)
+	vn.connect("music_ambience", music_ambience)
+	vn.connect("music_chiptune", music_chiptune)
+	vn.connect("music_bgm1", music_bgm1)
+	vn.connect("stop_music", stop_music)
+	vn.connect("sfx_truck", sfx_truck)
+	vn.connect("sfx_screams", sfx_screams)
 	
-	#signal start_music
+	
+#signal start_music
 #signal boost_stats
 #
 #signal music_ambience
@@ -28,11 +41,38 @@ func _ready():
 	#
 func fade_black():
 	EffectRegistry.start_effect(self, "ColorFade", [$MultiPassShaderRect.material])
+
 	
 func fade_red():
-	EffectRegistry.start_effect(self, "ColorFade", [$MultiPassShaderRect.material, Color(), Color()])
-	await get_tree().create_timer(0.5).timeout
-	EffectRegistry.start_effect(self, "ColorFade", [$MultiPassShaderRect.material, Color(), Color.TRANSPARENT])
+	EffectRegistry.start_effect(self, "ColorFade", [$MultiPassShaderRect.material, Color.TRANSPARENT, Color(176,11,30)])
+	await get_tree().create_timer(2).timeout
+	EffectRegistry.start_effect(self, "ColorFade", [$MultiPassShaderRect.material, Color(176,11,30), Color.TRANSPARENT])
 	
+func music_ambience():
+	print("music ambience")
+	audio.stream = song
+	audio.play()
 	
+func music_chiptune():
+	print("music chiptune")
+	audio.stream = song
+	audio.play()
+	
+func music_bgm1():
+	audio.stream = song
+	audio.play()
+	
+func stop_music():
+	audio.stop()
+	
+func sfx_truck():
+	sfx.stream = song
+	sfx.play()
+	
+func sfx_screams():
+	sfx.stream = song
+	sfx.play()
+	
+func name_player():
+	pass
 	
