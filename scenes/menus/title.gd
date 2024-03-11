72a5bc78bc46a9f620228316c10a53cf0eecc9e6
branden <brandenbohrnsen@gmail.com>
2024-03-11 15:35:15 -0700

    showcase build

--- scenes/menus/title.gd
+++ scenes/menus/title.gd
@@ -10,4 +10,24 @@ func _on_texture_button_pressed():
 	await EffectAnim.animation_finished
 	
 	get_tree().change_scene_to_packed(VN)
+
+func _on_library_pressed():
+	Util.create_minigame($CanvasLayer, "Library")
+
+func _on_fishing_pressed():
+	Util.create_minigame($CanvasLayer, "Fishing")
+
+func _on_rhythm_pressed():
+	Util.create_minigame($CanvasLayer, "Rhythm")
 	
+func _process(_delta):
+	if $CanvasLayer.get_child_count() > 0:
+		$TextureButton.disabled = true
+		$Library.disabled = true
+		$Fishing.disabled = true
+		$Rhythm.disabled = true
+	else:
+		$TextureButton.disabled = false
+		$Library.disabled = false
+		$Fishing.disabled = false
+		$Rhythm.disabled = false
