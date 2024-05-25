bfb56fa42759ae964b289faa5407b4d29d337571
branden <brandenbohrnsen@gmail.com>
2024-05-25 04:26:15 -0700

    im going to explode

--- tools/dialogue/SpriteHandler.gd
+++ tools/dialogue/SpriteHandler.gd
@@ -20,7 +20,17 @@ func init_sprite(character_list : Array[String], sprite_list : Array[String]):
 			get_node(name_string).text = focused_char
 			break
 		else:
-			focused_char = null
+			var temp = character_list.duplicate(true)
+			temp.erase("null")
+			temp.erase("null")
+			temp.erase("null")
+			temp.erase("null")
+			if temp.size() == 1:
+				focused_char = temp[0]
+				get_node(name_string).text = temp[0]
+				break
+			else:
+				focused_char = null
 			
 	for i in character_list.size():
 		character_list[i] = character_list[i].replace("!","")
@@ -31,6 +41,7 @@ func init_sprite(character_list : Array[String], sprite_list : Array[String]):
 		
 	for i : Character in CHARACTER_LIST.list:
 		if character_list.has(i.name):
+			print(i.name)
 			var a = CHARACTER_SPRITE.instantiate()
 			$HBoxContainer.add_child(a)
 			var texture : Texture
@@ -40,17 +51,37 @@ func init_sprite(character_list : Array[String], sprite_list : Array[String]):
 			for j in i.textures:
 				if j.resource_path.ends_with(sprite_list[
 					character_list.find(i.name)
-				]+ ".PNG"):
+				] + ".PNG"):
 					texture = j
 			
 			a.texture = texture
 			
+	## add nulls
+	
+	for i in character_list.size():
+		if character_list[i] == "null":
+			var a = CHARACTER_SPRITE.instantiate()
+			$HBoxContainer.add_child(a)
+			$HBoxContainer.move_child(
+				a, i
+			)
+			a.modulate.a = 0
+			
 	for i in $HBoxContainer.get_child_count():
 		if focused_char == character_list[i]:
-			$HBoxContainer.move_child(
-				$HBoxContainer.get_child(i), -1
-			)
+			#$HBoxContainer.get_child(i).z_index = 1
 			break
+			
+	for i in $HBoxContainer.get_child_count():
+		if focused_char != character_list[i-1]:
+			$HBoxContainer.get_child(i-1).modulate = Color("969696")
+		if character_list[i-1] != "null":
+			$HBoxContainer.get_child(i-1).modulate.a = 0
+			var q = create_tween()
+			q.tween_property($HBoxContainer.get_child(i-1),
+			"modulate:a", 1, 0.2)
+		else:
+			$HBoxContainer.get_child(i-1).modulate.a = 0
 				
 	match $HBoxContainer.get_child_count():
 		1:
