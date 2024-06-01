a135844b8b2a6233d112ebc50f494884f54382ab
branden <brandenbohrnsen@gmail.com>
2024-06-01 03:56:35 -0700

    stupid effect kill me holy shit

--- tools/dialogue/vn_dialogue.gd
+++ tools/dialogue/vn_dialogue.gd
@@ -419,6 +419,15 @@ func read_line(key : int):
 		EffectAnim.play("FadeBlackAbrupt")
 		await EffectAnim.animation_finished
 		get_tree().change_scene_to_file("res://scenes/menus/title.tscn")
+	elif current_line["flag"] == "menu_lace":
+		text_box.hide()
+		EffectAnim.play("FadeLace")
+		EffectAnim.speed_scale = 0.8
+		await EffectAnim.animation_finished
+		EffectAnim.speed_scale = 0.5
+		EffectAnim.play("FadeBlack")
+		await EffectAnim.animation_finished
+		get_tree().change_scene_to_file("res://scenes/menus/title.tscn")
 	elif current_line["flag"] == "quit":
 		get_tree().quit()
 	
