37f3e97f63504ed7888a257b1f6b077674010ae0
branden <brandenbohrnsen@gmail.com>
2024-06-07 03:09:03 -0700

    log?

--- tools/dialogue/log.gd
+++ tools/dialogue/log.gd
@@ -6,18 +6,12 @@ signal log_closed
 
 @onready var vbox_height = $ScrollContainer/VBoxContainer.size.y
 
-func init():
-	vbox_height = $ScrollContainer/VBoxContainer.size.y
-	scroll.value = 1
-	
-	if vbox_height <= 550:
-		scroll.hide()
-	else:
-		scroll.show()
-		$ScrollContainer.scroll_vertical = (vbox_height - 600) * scroll.value
+func _ready():
+	$VScrollBar.max_value = $ScrollContainer.get_v_scroll_bar().max_value
+	$VScrollBar.min_value = $ScrollContainer.get_v_scroll_bar().min_value
 
 func _on_v_scroll_bar_value_changed(value):
-	$ScrollContainer.scroll_vertical = (vbox_height - 600) * value
+	$ScrollContainer.scroll_vertical = value
 
 func _on_texture_button_pressed():
 	log_closed.emit()
